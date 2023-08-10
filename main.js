3a875aa2d08ddffe85608d074fd23602e42333fa
PuffCab <berlinkollner@gmail.com>
2023-08-10 12:52:13 +0200

    for loop working

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log("I know some Javascript");
+
+const sentences = ["I", "don't know", "python"];
+for (let index = 0; index < sentences.length; index++) {
+  const element = sentences[index];
+}
